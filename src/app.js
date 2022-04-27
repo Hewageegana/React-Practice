3f4958f1c940340b67df8bb34e91ad6e4c00bfe0
“rusiru2000hewageegana@gmail.com” <rusiru2000hewageegana@gmail.com>
2022-04-27 16:53:26 +0530

    create server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const PORT = "8070";
+
+app.use(cors());
+app.use(express.json({ limit: "20mb" }));
+
+app.get("/", (req, res, next) => {
+  res.send("<h2> Library Management System</h2>");
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`server is up & running on PORT ${PORT}`);
+});
